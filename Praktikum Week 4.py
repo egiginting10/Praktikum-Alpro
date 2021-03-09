d5090b67ef85b140de9785549150952525627a7b
egiginting10 <78341282+egiginting10@users.noreply.github.com>
2021-03-09 17:23:15 +0700

    Add files via upload

---
+++ Praktikum Week 4.py
@@ -0,0 +1,22 @@
+#Nama : Egi Granaldi Ginting
+#NIM  : 71200634
+#Universitas Kristen Duta Wacana
+#Sumber : Saya sendiri
+
+'''
+Buatlah sebuah program perulangan yang akan mengetahui apakah bilangan A kurang dari 
+atau tidak kurang dari menggunakan perulangan while
+'''
+'''
+Input   = Bilangan A
+
+Proses  = Mengulang bilangan A
+
+Output  = Menampilkan bilangan dari proses perulangan
+'''
+A = int(input("Masukkan Bilangan A : "))
+while (A < 5):
+    print(A, "kurang dari 5")
+    A = A + 1
+else:
+    print(A, "tidak kurang dari 5")
